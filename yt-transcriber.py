7ac4fcda80a03e2d1ea992edff36d6ce0458e300
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2023-11-20 20:14:24 +0800

    Update to add logging and deletion of audio chunks

--- yt-transcriber.py
+++ yt-transcriber.py
@@ -4,6 +4,8 @@ OpenAI Whisper API.
 """
 
 import argparse
+import logging
+import os
 from pytube import YouTube
 from pydub import AudioSegment
 from openai import OpenAI
@@ -11,16 +13,45 @@ from openai import OpenAI
 
 def get_args() -> argparse.Namespace:
     """
-    Get the command-line arguments.
+    Parse command-line arguments.
 
     Returns:
-        argparse.Namespace: The parsed arguments.
+        argparse.Namespace: The parsed arguments including video URL, flag for
+        keeping audio files, and flag for enabling logging.
     """
     parser = argparse.ArgumentParser(
-        description="Transcribe a YouTube video using OpenAI Whisper API."
+        description="Transcribe a YouTube video using OpenAI Whisper API.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("video_url", type=str, help="URL of the YouTube video.")
+    parser.add_argument(
+        "video_url",
+        type=str,
+        help="URL of the YouTube video.",
+    )
+    parser.add_argument(
+        "--keep_audio",
+        action="store_true",
+        help="Keep audio files after transcription.",
+    )
+    parser.add_argument(
+        "--enable_logging",
+        action="store_true",
+        help="Enable logging output.",
+    )
     return parser.parse_args()
+
+
+def setup_logging(enable: bool):
+    """
+    Set up logging configuration based on the command-line argument.
+
+    Args:
+        enable (bool): Flag to enable or disable logging.
+    """
+    if enable:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.WARNING)
 
 
 def get_audio(video_url: str) -> str:
@@ -31,28 +62,28 @@ def get_audio(video_url: str) -> str:
         video_url (str): URL of the YouTube video.
 
     Returns:
-        str: Path to the downloaded audio.
+        str: Path to the downloaded audio file.
     """
-    print(f"Downloading audio from {video_url}")
+    logging.info("Downloading audio from %s", video_url)
     yt = YouTube(video_url)
     audio_stream = yt.streams.filter(only_audio=True).first()
     output_path = audio_stream.download()
-    print(f"Audio downloaded to {output_path}")
+    logging.info("Audio downloaded to %s", output_path)
     return output_path
 
 
 def split_audio(audio_path: str, chunk_length_min: int = 5) -> list[str]:
     """
-    Split the audio into 5-minute chunks.
+    Split the audio into chunks of a specified length.
 
     Args:
         audio_path (str): Path to the audio file.
-        chunk_length_min (int): Length of each audio chunk in minutes.
+        chunk_length_min (int): Length of each audio chunk in minutes. Default is 5 minutes.
 
     Returns:
         list[str]: List of paths to the audio chunks.
     """
-    print(f"Splitting audio into {chunk_length_min}-minute chunks")
+    logging.info("Splitting audio into %s-minute chunks", chunk_length_min)
     file_ext = audio_path.split(".")[-1]
     audio = AudioSegment.from_file(audio_path)
     chunk_length_ms = chunk_length_min * 60 * 1000
@@ -62,29 +93,32 @@ def split_audio(audio_path: str, chunk_length_min: int = 5) -> list[str]:
         chunk_path = f"chunk_{i // chunk_length_ms}.{file_ext}"
         chunk.export(chunk_path, format=file_ext)
         chunks.append(chunk_path)
-    print(f"Audio split into {len(chunks)} chunks")
+    logging.info("Audio split into %d chunks", len(chunks))
     return chunks
 
 
-def transcribe_audio_chunk(client: OpenAI, audio_chunk_path: str) -> str:
+def transcribe_audio_chunks(client: OpenAI, audio_chunks: list[str]) -> str:
     """
-    Transcribe an audio chunk using OpenAI API.
+    Transcribe all audio chunks using OpenAI API and return the combined transcription.
 
     Args:
         client (OpenAI): OpenAI API client.
-        audio_chunk_path (str): Path to the audio chunk.
+        audio_chunks (list[str]): List of paths to the audio chunks.
 
     Returns:
-        str: Transcription of the audio chunk.
+        str: Combined transcription of all audio chunks.
     """
-    print(f"Transcribing {audio_chunk_path}")
-    with open(audio_chunk_path, "rb") as audio_file:
-        transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio_file,
-        )
-    print(f"Transcription: {transcript.text}")
-    return transcript.text
+    transcriptions = []
+    for audio_chunk in audio_chunks:
+        logging.info("Transcribing %s", audio_chunk)
+        with open(audio_chunk, "rb") as audio_file:
+            transcript = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_file,
+            )
+        logging.info("Transcription obtained")
+        transcriptions.append(transcript.text)
+    return "\n".join(transcriptions)
 
 
 def save_transcript(transcript: str, output_path: str) -> None:
@@ -92,23 +126,35 @@ def save_transcript(transcript: str, output_path: str) -> None:
     Save the transcript to a file.
 
     Args:
-        transcript (str): The transcript.
+        transcript (str): The combined transcript.
         output_path (str): Path to the output file.
     """
-    print(f"Saving transcript to {output_path}")
+    logging.info("Saving transcript to %s", output_path)
     with open(output_path, "w", encoding="utf-8") as f:
         f.write(transcript)
-    print("Transcript saved")
+    logging.info("Transcript saved")
+
+
+def cleanup_audio_files(audio_chunks: list[str], keep_audio: bool):
+    """
+    Delete audio chunk files unless the keep_audio flag is set.
+
+    Args:
+        audio_chunks (list[str]): List of audio chunk file paths.
+        keep_audio (bool): Flag indicating whether to keep audio files after transcription.
+    """
+    if not keep_audio:
+        for audio_chunk in audio_chunks:
+            os.remove(audio_chunk)
+        logging.info("Audio chunk files deleted.")
 
 
 if __name__ == "__main__":
     args = get_args()
+    setup_logging(args.enable_logging)
     audio_path = get_audio(args.video_url)
     audio_chunks = split_audio(audio_path)
     client = OpenAI()
-    transcriptions = []
-
-    for audio_chunk in audio_chunks:
-        transcriptions.append(transcribe_audio_chunk(client, audio_chunk))
-
-    save_transcript("\n".join(transcriptions), "transcript.txt")
+    transcript = transcribe_audio_chunks(client, audio_chunks)
+    save_transcript(transcript, "transcript.txt")
+    cleanup_audio_files(audio_chunks, args.keep_audio)
